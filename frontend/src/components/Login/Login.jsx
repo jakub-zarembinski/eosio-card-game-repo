54c22aa194623dcad0b8adc54ab3734dd3b8eb27
jakub-zarembinski <jakub.zarembinski@tokenika.io>
2019-10-17 16:42:28 +0200

    xxx

--- frontend/src/components/Login/Login.jsx
+++ frontend/src/components/Login/Login.jsx
@@ -15,9 +15,9 @@ class Login extends Component {
     this.state = {
       form: {
         username: '',
-        key: ''
+        key: '',
       },
-      error: ''
+      error: '',
     }
     // Bind functions
     this.handleChange = this.handleChange.bind(this);
@@ -32,9 +32,9 @@ class Login extends Component {
     this.setState({
       form: {
         ...form,
-        [name]: value
+        [name]: value,
       },
-      error: ''
+      error: '',
     });
   }
 
